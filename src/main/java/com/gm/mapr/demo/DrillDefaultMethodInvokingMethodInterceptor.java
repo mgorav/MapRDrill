597ed69711ee3e6f56833413e5a4d3c86de5da2a
mgorav <mgorav@gmail.com>
2018-07-03 10:52:27 +0200

    Spring data way

---
+++ src/main/java/com/gm/mapr/demo/DrillDefaultMethodInvokingMethodInterceptor.java
@@ -0,0 +1,63 @@
+package com.gm.mapr.demo;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.projection.DefaultMethodInvokingMethodInterceptor;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
+import org.springframework.data.repository.core.RepositoryMetadata;
+import org.springframework.data.repository.core.support.DefaultRepositoryMetadata;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.ResultSetWrappingSqlRowSet;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.lang.reflect.Method;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DrillDefaultMethodInvokingMethodInterceptor extends DefaultMethodInvokingMethodInterceptor {
+    RepositoryMetadata metadata = new DefaultRepositoryMetadata(DrillRepository.class);
+
+    ProjectionFactory factory = new SpelAwareProxyProjectionFactory();
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+
+        List<YelpObject> yelpObjects = new ArrayList<>();
+
+        DrillQueryMethod method  = getQueryMethod(DrillRepository.class, invocation.getMethod().getName(),invocation.getMethod().getParameterTypes());
+
+        String sql = method.getAnnotatedQuery();
+
+        int cnt = 0;
+        for (Object argument : invocation.getArguments()) {
+             String name = method.getParameters().getBindableParameter(cnt).getName().get();
+             sql = sql.replaceAll("\\?"+name, argument.toString());
+             cnt++;
+        }
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        ResultSet result = ((ResultSetWrappingSqlRowSet) results).getResultSet();
+
+        while (result.next()) {
+
+            yelpObjects.add(new YelpObject(result.getString(1), result.getString(2), result.getString(3)));
+        }
+
+        return yelpObjects;
+
+//        return super.invoke(invocation);
+    }
+
+
+    private DrillQueryMethod getQueryMethod(Class<?> repositoryInterface, String methodName, Class<?>... parameterTypes)
+            throws Exception {
+
+        Method method = repositoryInterface.getMethod(methodName, parameterTypes);
+        return new DrillQueryMethod(method, metadata, factory);
+    }
+}
